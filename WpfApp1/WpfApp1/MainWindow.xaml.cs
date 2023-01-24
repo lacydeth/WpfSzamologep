25666f9831384a4ee195ea1deb624f4aecfd96fd
lacydeth <57276660+lacydeth@users.noreply.github.com>
2023-01-24 03:06:52 +0100

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOsszead_Click(object sender, RoutedEventArgs e)
+        {
+            String eredmeny = $"{txtAszam.Text} + {txtBszam.Text} = {Convert.ToDouble(txtAszam.Text) + Convert.ToDouble(txtBszam.Text)}";
+            MessageBox.Show(eredmeny);
+            lbEredmenyek.Items.Add(eredmeny);
+        }
+
+        private void btnKivon_Click(object sender, RoutedEventArgs e)
+        {
+            String eredmeny = $"{txtAszam.Text} - {txtBszam.Text} = {Convert.ToDouble(txtAszam.Text) - Convert.ToDouble(txtBszam.Text)}";
+            MessageBox.Show(eredmeny);
+            lbEredmenyek.Items.Add(eredmeny);
+        }
+
+        private void btnSzoroz_Click(object sender, RoutedEventArgs e)
+        {
+            String eredmeny = $"{txtAszam.Text} * {txtBszam.Text} = {Convert.ToDouble(txtAszam.Text) * Convert.ToDouble(txtBszam.Text)}";
+            MessageBox.Show(eredmeny);
+            lbEredmenyek.Items.Add(eredmeny);
+        }
+
+        private void btnOszt_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToDouble(txtBszam.Text) == 0)
+            {
+                MessageBox.Show("0-val nem osztunk!");
+            }
+            else
+            {
+                String eredmeny = $"{txtAszam.Text} / {txtBszam.Text} = {Convert.ToDouble(txtAszam.Text) / Convert.ToDouble(txtBszam.Text)}";
+                MessageBox.Show(eredmeny);
+                lbEredmenyek.Items.Add(eredmeny);
+            }
+        }
+    }
+}
